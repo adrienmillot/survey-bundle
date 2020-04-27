16a3efa64cfd39c1a2553abd471e98c7b222aca7
Adrien <adrien.millot@gmail.com>
2020-04-27 22:01:52 +0200

    Bundle initialize

---
+++ SurveyBundle.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace amillot\SurveyBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class SurveyBundle
+ *
+ * @author Adrien MILLOT <adrien.millot@gmail.com>
+ * @license For the full copyright and license information, please view the LICENSE
+ *          file that was distributed with this source code.
+ */
+class SurveyBundle extends Bundle
+{
+}
