9ac811e338880c418d645dded64d1255f6b784c7
Adrien <adrien.millot@gmail.com>
2020-04-29 16:08:38 +0200

    Add Answer entity

--- Model/ProposalInterface.php
+++ Model/ProposalInterface.php
@@ -4,12 +4,14 @@ declare(strict_types=1);
 
 namespace amillot\SurveyBundle\Model;
 
+use amillot\SurveyBundle\Model\Traits\AnswerListAwareInterface;
 use amillot\SurveyBundle\Model\Traits\LabellableInterface;
 use amillot\SurveyBundle\Model\Traits\QuestionAwareInterface;
 use amillot\SurveyBundle\Model\Traits\ValidableInterface;
 use amillot\SurveyBundle\Model\Traits\WeightableInterface;
 
-interface ProposalInterface extends LabellableInterface,
+interface ProposalInterface extends AnswerListAwareInterface,
+                                    LabellableInterface,
                                     QuestionAwareInterface,
                                     ValidableInterface,
                                     WeightableInterface
