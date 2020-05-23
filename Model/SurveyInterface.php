3117343029ea791929f3d3aec31d04bb2a713048
Adrien <adrien.millot@gmail.com>
2020-05-23 19:25:50 +0200

    Add Survey subject interface

--- Model/SurveyInterface.php
+++ Model/SurveyInterface.php
@@ -8,4 +8,15 @@ use amillot\SurveyBundle\Model\Traits\LabellableInterface;
 
 interface SurveyInterface extends LabellableInterface
 {
+    /**
+     * @return SurveySubjectInterface|null
+     */
+    public function getAuthor(): ?SurveySubjectInterface;
+
+    /**
+     * @param SurveySubjectInterface|null $prmAuthor
+     *
+     * @return SurveyInterface
+     */
+    public function setAuthor(?SurveySubjectInterface $prmAuthor): SurveyInterface;
 }
