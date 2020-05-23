5294a670d908c59164d090693c3e85bedeac7ce2
Adrien <adrien.millot@gmail.com>
2020-05-23 19:32:13 +0200

    Add Question subject interface

--- Entity/AbstractQuestion.php
+++ Entity/AbstractQuestion.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace amillot\SurveyBundle\Entity;
 
 use amillot\SurveyBundle\Model\QuestionInterface;
+use amillot\SurveyBundle\Model\QuestionSubjectInterface;
 use amillot\SurveyBundle\Traits\Labellable;
 use amillot\SurveyBundle\Traits\SurveyAware;
 use amillot\SurveyBundle\Traits\Weightable;
@@ -12,6 +13,8 @@ use amillot\SurveyBundle\Traits\Weightable;
 abstract class AbstractQuestion implements QuestionInterface
 {
     use Labellable, SurveyAware, Weightable;
+
+    protected $author;
 
     protected $id;
 
@@ -31,5 +34,23 @@ abstract class AbstractQuestion implements QuestionInterface
     public function getId()
     {
         return $this->id;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getAuthor(): ?QuestionSubjectInterface
+    {
+        return $this->author;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function setAuthor(?QuestionSubjectInterface $prmAuthor): QuestionInterface
+    {
+        $this->author = $prmAuthor;
+
+        return $this;
     }
 }
