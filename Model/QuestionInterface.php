6331aaee36f475beee865fde2fc4c9ff2feb9e55
Adrien <adrien.millot@gmail.com>
2020-05-23 19:33:42 +0200

    Add Category entity

--- Model/QuestionInterface.php
+++ Model/QuestionInterface.php
@@ -4,11 +4,13 @@ declare(strict_types=1);
 
 namespace amillot\SurveyBundle\Model;
 
+use amillot\SurveyBundle\Model\Traits\CategoryAwareInterface;
 use amillot\SurveyBundle\Model\Traits\LabellableInterface;
 use amillot\SurveyBundle\Model\Traits\SurveyAwareInterface;
 use amillot\SurveyBundle\Model\Traits\WeightableInterface;
 
-interface QuestionInterface extends LabellableInterface,
+interface QuestionInterface extends CategoryAwareInterface,
+                                    LabellableInterface,
                                     SurveyAwareInterface,
                                     WeightableInterface
 {
