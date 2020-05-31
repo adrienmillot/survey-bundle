2895341596d5e11a31da62afbd9a14d2ed47c0a2
Adrien <adrien.millot@gmail.com>
2020-05-31 11:53:21 +0200

    Add Question subject interface

---
+++ Model/QuestionSubjectInterface.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace amillot\SurveyBundle\Model;
+
+use amillot\SurveyBundle\Model\Traits\QuestionListAwareInterface;
+
+interface QuestionSubjectInterface extends QuestionListAwareInterface
+{
+}
